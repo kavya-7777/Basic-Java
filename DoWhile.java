4e53f7e827d931d1b156fc22e5f956fa82bc0038
KAVYA M <kavya77.m@gmail.com>
2025-06-22 18:15:33 +0530

    Do-while Loops

---
+++ DoWhile.java
@@ -0,0 +1,9 @@
+class DoWhile{
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            System.out.print("Hello, World!");
+            i++;
+        }while(i>1);
+    }
+}
---
+++ NestedDoWhile.java
@@ -0,0 +1,14 @@
+public class NestedDoWhile{
+    public static void main(String[] args){
+        int i =1;
+        do{
+            int j =1;
+            do{
+                System.out.print("("+i+","+j+") ");
+                j++;
+            }while(j<=3);
+            System.out.println();
+            i++;
+        }while(i<=3);
+    }
+}
