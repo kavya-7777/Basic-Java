29836238beb67340365034aef7b1b15b8505cac0
KAVYA M <kavya77.m@gmail.com>
2025-07-26 10:38:41 +0530

    Alphabet Patterns

---
+++ Patterns/AlphaPatterns/LeftAligned.java
@@ -0,0 +1,11 @@
+public class LeftAligned{
+    public static void main(String[] args){
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print((char)(65 + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
